787b31a60fc794d397e67bf849b1a654106687de
iwatakeshi <iwatakeshi@users.noreply.github.com>
2014-08-30 18:42:33 -0600

    Update Reamdme, fix version

--- gengo.js
+++ gengo.js
@@ -4,7 +4,7 @@
 /*global module, $, console*/
 /*
  * gengojs
- * version : 0.1.8
+ * version : 0.1.9
  * author : Takeshi Iwana
  * https://github.com/iwatakeshi
  * license : MIT
@@ -49,7 +49,7 @@
         vsprintf = require("sprintf-js").vsprintf,
         // check for nodeJS
         hasModule = (typeof module !== 'undefined' && module.exports),
-        VERSION = '0.1.8',
+        VERSION = '0.1.9',
         //configuration with defaults set
         CONFIG = {
             //path to locales
